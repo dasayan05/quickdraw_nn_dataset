fcb5588ffe414968125f0d6acc53abd44acfb498
Das, Ayan (PG/R - Elec Electronic Eng) <a.das@surrey.ac.uk>
2019-11-22 17:01:02 +0000

    Lots of Modifications. 1. Just one file now; contains everything 2. Add two modes - sketch and stroke 3. have options for spliting out the pen-up probs 4. have options for having shifted sequence as supervision

--- quickdraw.py
+++ quickdraw.py
@@ -2,12 +2,48 @@ import os, random, pdb
 import torch, numpy as np
 from torch.utils.data import Dataset, DataLoader
 from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence
-from .utils import unpack_drawing, struct
+
+import struct
+from struct import unpack
+
+def unpack_drawing(file_handle):
+    '''
+    Utility function from QuickDraw project. Originally taken from ..
+    https://github.com/googlecreativelab/quickdraw-dataset/blob/master/examples/binary_file_parser.py
+    '''
+    key_id, = unpack('Q', file_handle.read(8))
+    countrycode, = unpack('2s', file_handle.read(2))
+    recognized, = unpack('b', file_handle.read(1))
+    timestamp, = unpack('I', file_handle.read(4))
+    n_strokes, = unpack('H', file_handle.read(2))
+    image = []
+    for i in range(n_strokes):
+        n_points, = unpack('H', file_handle.read(2))
+        fmt = str(n_points) + 'B'
+        x = unpack(fmt, file_handle.read(n_points))
+        y = unpack(fmt, file_handle.read(n_points))
+        image.append((x, y))
+    
+    return {
+        'key_id': key_id,
+        'countrycode': countrycode,
+        'recognized': recognized,
+        'timestamp': timestamp,
+        'image': image
+    }
 
 class QuickDraw(Dataset):
-    def __init__(self, root, categories=[], max_samples=80000, normalize_xy=True, dtype=np.float32, verbose=False,
-            *, cache=None, # not to be used
-            filter_func=None # subset sketches based on a function
+
+    # Two modes of working
+    SKETCH = 0
+    STROKE = 1
+
+    def __init__(self, root, *, categories=[], max_samples=80000, normalize_xy=True, dtype=np.float32, verbose=False,
+            cache=None, # not to be used
+            filter_func=None, # subset sketches based on a function
+            mode=SKETCH, # default in sketch mode
+            seperate_p_tensor=False,
+            shifted_seq_as_supevision=False
         ):
         super().__init__()
 
@@ -25,9 +61,12 @@ class QuickDraw(Dataset):
         self.verbose = verbose
         self.max_samples = max_samples
         if filter_func == None:
-            self.filter_func = lambda sk: (True, sk) # passes every sketch
+            self.filter_func = lambda s: (True, s) # passes every sketch
         else:
             self.filter_func = filter_func
+        self.mode = mode
+        self.seperate_p_tensor = seperate_p_tensor
+        self.shifted_seq_as_supevision = shifted_seq_as_supevision
 
         # The cached data
         if cache != None:
@@ -41,25 +80,42 @@ class QuickDraw(Dataset):
                     while True:
                         try:
                             drawing = unpack_drawing(file)['image']
-                            
-                            # Passes all sketches through 'filter_func'. It returns either
+                            # breakpoint()
+
+                            # Passes all sketches/strokes through 'filter_func'. It returns either
                             # (True, modified_sketch) OR (False, <anything>). If the first return
                             # object is True, it adds the 'modified_sketch' into cache. If False,
                             # it just skips that sample (the 2nd argument is useless then).
-                            filter_check, _drawing = self.filter_func(drawing)
-                            if filter_check:
-                                drawing = _drawing
-                            else:
-                                continue
+                            if self.mode == QuickDraw.SKETCH:
+                                filter_check, _drawing = self.filter_func(drawing)
+                                if filter_check:
+                                    drawing = _drawing
+                                else:
+                                    continue
+                                
+                                # Append the whole sketch (with category ID)
+                                self.cache.append((drawing, cat_idx))
+                                n_samples += 1
+
+                            elif self.mode == QuickDraw.STROKE:
+                                stroke_drawings = [([d,], cat_idx) for d in drawing]
+                                for j, (sd, _) in enumerate(stroke_drawings):
+                                    filter_check, _sd = self.filter_func(sd)
+                                    if filter_check:
+                                        stroke_drawings[j] = (_sd, cat_idx)
+                                    else:
+                                        continue
+
+                                # Append the stroke sketchs (with category ID each)
+                                self.cache.extend(stroke_drawings)
+                                n_samples += len(stroke_drawings)
                             
-                            self.cache.append((drawing, cat_idx))
-                            n_samples += 1
                             if n_samples >= max_samples:
                                 break
                         except struct.error:
                             break
                 if self.verbose:
-                    print('[Info] {} sketches read from {}'.format(n_samples, bin_file_path))
+                    print('[Info] {} sketches/strokes read from {}'.format(n_samples, bin_file_path))
 
             random.shuffle(self.cache)
 
@@ -87,16 +143,53 @@ class QuickDraw(Dataset):
             norm_factor = np.sqrt((sketch[:,:2]**2).sum(1)).max()
             sketch[:,:2] = sketch[:,:2] / (norm_factor + np.finfo(self.dtype).eps)
 
-        return sketch, c_id
+        if self.seperate_p_tensor:
+            if self.shifted_seq_as_supevision:
+                return (sketch[:-1,:-1], sketch[:-1,-1]), (sketch[1:,:-1], sketch[1:,-1]), c_id
+            else:
+                return (sketch[:,:-1], sketch[:,-1]), c_id
+        else:
+            if self.shifted_seq_as_supevision:
+                return sketch[:-1,:], sketch[1:,:], c_id
+            else:
+                return sketch, c_id
 
-    def collate(batch):
-        lengths = torch.tensor([x.shape[0] for (x, _) in batch])
-        padded_seq_inp = pad_sequence([torch.tensor(x) for (x, _) in batch])
-        labels = torch.tensor([c for (_, c) in batch])
-        return pack_padded_sequence(padded_seq_inp, lengths, enforce_sorted=False), labels
+    def collate(self, batch):
+        if self.seperate_p_tensor:
+            if self.shifted_seq_as_supevision:
+                lengths = torch.tensor([x.shape[0] for (x, _), (_, _), _ in batch])
+                padded_seq_inp = pad_sequence([torch.tensor(x) for (x, _), (_, _), _ in batch])
+                padded_seq_inp_p = pad_sequence([torch.tensor(p) for (_, p), (_, _), _ in batch])
+                padded_seq_shifted = pad_sequence([torch.tensor(x) for (_, _), (x, _), _ in batch])
+                padded_seq_shifted_p = pad_sequence([torch.tensor(p) for (_, _), (_, p), _ in batch])
+                labels = torch.tensor([c for (_, _), (_, _), c in batch])
+                return (pack_padded_sequence(padded_seq_inp, lengths, enforce_sorted=False),\
+                        pack_padded_sequence(padded_seq_inp_p, lengths, enforce_sorted=False)),\
+                       (pack_padded_sequence(padded_seq_shifted, lengths, enforce_sorted=False), \
+                        pack_padded_sequence(padded_seq_shifted_p, lengths, enforce_sorted=False)), labels
+            else:
+                lengths = torch.tensor([x.shape[0] for (x, _), _ in batch])
+                padded_seq_inp = pad_sequence([torch.tensor(x) for (x, _), _ in batch])
+                padded_seq_inp_p = pad_sequence([torch.tensor(p) for (_, p), _ in batch])
+                labels = torch.tensor([c for (_, _), c in batch])
+                return (pack_padded_sequence(padded_seq_inp, lengths, enforce_sorted=False),\
+                        pack_padded_sequence(padded_seq_inp_p, lengths, enforce_sorted=False)), labels
+        else:
+            if self.shifted_seq_as_supevision:
+                lengths = torch.tensor([x.shape[0] for (x, _, _) in batch])
+                padded_seq_inp = pad_sequence([torch.tensor(x) for (x, _, _) in batch])
+                padded_seq_shifted = pad_sequence([torch.tensor(x) for (_, x, _) in batch])
+                labels = torch.tensor([c for (_, _, c) in batch])
+                return pack_padded_sequence(padded_seq_inp, lengths, enforce_sorted=False),\
+                        pack_padded_sequence(padded_seq_shifted, lengths, enforce_sorted=False), labels
+            else:
+                lengths = torch.tensor([x.shape[0] for (x, _) in batch])
+                padded_seq_inp = pad_sequence([torch.tensor(x) for (x, _) in batch])
+                labels = torch.tensor([c for (_, c) in batch])
+                return pack_padded_sequence(padded_seq_inp, lengths, enforce_sorted=False), labels
 
     def get_dataloader(self, batch_size, shuffle = True, pin_memory = True):
-        return DataLoader(self, batch_size=batch_size, collate_fn=QuickDraw.collate, shuffle=shuffle, pin_memory=pin_memory)
+        return DataLoader(self, batch_size=batch_size, collate_fn=self.collate, shuffle=shuffle, pin_memory=pin_memory)
 
     def split(self, proportion=0.8):
         train_samples = int(len(self) * proportion)
@@ -112,7 +205,8 @@ class QuickDraw(Dataset):
 if __name__ == '__main__':
     import sys
 
-    qd = QuickDraw(sys.argv[1], categories=['airplane', 'bus'], max_samples=1000, verbose=True)
+    qd = QuickDraw(sys.argv[1], categories=['airplane', 'bus'], max_samples=2, verbose=True, mode=QuickDraw.SKETCH,
+        seperate_p_tensor=False, shifted_seq_as_supevision=True)
     qdl = qd.get_dataloader(4)
-    for X, Y in qdl:
-        print(Y); break
+    for X, Y, _ in qdl:
+        breakpoint()
